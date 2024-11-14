5a013b43f247ff1a4a580b4fdad8098de556d3d8
Edward Schiller <107529923+edbleistift@users.noreply.github.com>
2024-11-14 20:10:38 +0500

    Update model.py

--- model.py
+++ model.py
@@ -4,29 +4,23 @@ from torch.utils.data import DataLoader, Dataset
 from sklearn.model_selection import train_test_split
 from transformers import BertTokenizer, BertForSequenceClassification, AdamW
 
-# Загрузка и предобработка данных
 file_path = 'cleaned_kaspi_reviews.csv'
 print("Загрузка данных...")
 df = pd.read_csv(file_path)
 
-# Удаление ненужных столбцов
-df = df.drop(columns=['Unnamed: 0.1', 'Unnamed: 0'])
+df = df.drop(columns=['Unnamed: 0.1', 'Unnamed: 0']) # Удаление ненужных столбцов
 
-# Фильтрация по рейтингу и языку
 print("Фильтрация и создание меток...")
-df = df[df['rating'].isin([1, 2, 4, 5])]  # Убираем нейтральные оценки
+df = df[df['rating'].isin([1, 2, 4, 5])]  # Удаление нейтральных оценок
 df = df[df['language'] == 'russian']  # Оставляем только русскоязычные комментарии
 df['label'] = df['rating'].apply(lambda x: 0 if x <= 2 else 1)  # 0 - плохие, 1 - хорошие
 
-# Удаляем строки с пустыми значениями в combined_text
-df = df.dropna(subset=['combined_text'])
+df = df.dropna(subset=['combined_text']) # Удаление строк с пустыми значениями в combined_text
 print(f"Количество строк после фильтрации: {len(df)}")
 
-# Разделение данных на обучающую и тестовую выборки
 print("Разделение данных на обучающую и тестовую выборки...")
 train_texts, val_texts, train_labels, val_labels = train_test_split(df['combined_text'], df['label'], test_size=0.2)
 
-# Токенизация и преобразование текста
 print("Токенизация текстов...")
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 train_encodings = tokenizer(list(train_texts), truncation=True, padding=True, max_length=128)
@@ -49,11 +43,9 @@ print("Создание датасетов...")
 train_dataset = ReviewDataset(train_encodings, train_labels.tolist())
 val_dataset = ReviewDataset(val_encodings, val_labels.tolist())
 
-# Инициализация модели
 print("Инициализация модели...")
 model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=2)
 
-# Настройка тренировки
 print("Настройка параметров тренировки...")
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 model.to(device)
@@ -64,7 +56,7 @@ optimizer = AdamW(model.parameters(), lr=1e-5)
 # Функция для тренировки
 def train_model():
     model.train()
-    for epoch in range(3):  # 3 эпохи обучения
+    for epoch in range(3):
         total_loss = 0
         for i, batch in enumerate(train_loader):
             batch = {k: v.to(device) for k, v in batch.items()}
@@ -80,11 +72,9 @@ def train_model():
         
         print(f'Epoch {epoch + 1} - Средняя потеря: {total_loss / len(train_loader):.4f}')
 
-# Тренировка модели
 print("Начало тренировки модели...")
 train_model()
 
-# Оценка точности
 print("Оценка модели на тестовой выборке...")
 model.eval()
 correct = 0
